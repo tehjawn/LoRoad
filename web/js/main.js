bbe45d10904ee1078e311e45325237f274b8ae27
jn23 <tehjawn@gmail.com>
2017-06-10 16:53:28 -0400

    Wow such data

--- web/js/main.js
+++ web/js/main.js
@@ -2,20 +2,31 @@
 
 var cb = new ClearBlade();
 var initOptions = {
-    URI : "https://machineq.clearblade.com",
-    messagingURI : "machineq.clearblade.com",
+    URI: "https://machineq.clearblade.com",
+    messagingURI: "machineq.clearblade.com",
     messagingPort: 8904,
     useMQTT: true,
     cleanSession: true,
     systemKey: "fe84ba920bce84d088a1ded934",
     systemSecret: "FE84BA920BFCD28284EDD5FD9AB601",
-    email: "crystalfruit.john@gmail.com",
-    password: "makingbacon"
+    email: "hi@crystalfruit.co",
+    password: "supsupsup"
 }
 
 cb.init(initOptions);
 
 // ---- //
+
+// Helper
+function hex2dec(hex) {
+    hex = hex.split(/\./);
+    var len = hex[1].length;
+    hex[1] = parseInt(hex[1], 16);
+    hex[1] *= Math.pow(16, -len);
+    return parseInt(hex[0], 16) + hex[1];
+}
+
+// --- //
 
 $('.button-collapse').sideNav();
 
@@ -37,6 +48,22 @@ var marker = L.marker([39.953110, -75.172455]).addTo(potholeMap);
 
 function refreshMarkers() {
     alert('refreshing markers~')
+    var query = cb.Query({ collectionName: "Sensor Data" });
+    query.setPage(1, 0);
+    query.fetch(function (err, data) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(data);
+            for(var i = 0; i < data.length; i++) {
+                var randLon = (Math.random() * (0.12) + 39.897642).toFixed(4)
+                var randLat = (Math.random() * (0.12) + 75.132877).toFixed(4) * -1
+                L.marker([randLon, randLat]).addTo(potholeMap)
+            }
+        }
+    })
+
 }
 
 
@@ -55,8 +82,24 @@ var polygon = L.polygon([
     [39.954820, -75.151098],
     [39.951004, -75.151999]
 ], {
-    color: "orange"
-}).addTo(predictionMap);
+        color: "orange"
+    }).addTo(predictionMap);
+
+var polygon2 = L.polygon([
+    [39.944520, -75.151098],
+    [39.934510, -75.151048],
+    [39.941504, -75.151989]
+], {
+        color: "orange"
+    }).addTo(predictionMap);
+
+var polygon3 = L.polygon([
+    [39.944520, -75.251098],
+    [39.934510, -75.231048],
+    [39.941504, -75.201989]
+], {
+        color: "orange"
+    }).addTo(predictionMap);
 
 function refreshAreas() {
     alert('refreshing areas~')
